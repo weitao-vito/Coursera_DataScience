184199f606f0bbeaa0166d4fe54c3ee84ef2e867
Weitao Chen <56017567+weitao-vito@users.noreply.github.com>
2023-05-21 11:01:55 +0200

    Add files via upload

---
+++ stock/gmestock.py
@@ -0,0 +1,46 @@
+#%%
+import yfinance as yf
+import pandas as pd
+import requests 
+from bs4 import BeautifulSoup 
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+def make_graph(stock_data, revenue_data, stock):
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, subplot_titles=("Historical Share Price", "Historical Revenue"), vertical_spacing = .3)
+    stock_data_specific = stock_data[stock_data.Date <= '2021--06-14']
+    revenue_data_specific = revenue_data[revenue_data.Date <= '2021-04-30']
+    fig.add_trace(go.Scatter(x=pd.to_datetime(stock_data_specific.Date, infer_datetime_format=True), y=stock_data_specific.Close.astype("float"), name="Share Price"), row=1, col=1)
+    fig.add_trace(go.Scatter(x=pd.to_datetime(revenue_data_specific.Date, infer_datetime_format=True), y=revenue_data_specific.Revenue.astype("float"), name="Revenue"), row=2, col=1)
+    fig.update_xaxes(title_text="Date", row=1, col=1)
+    fig.update_xaxes(title_text="Date", row=2, col=1)
+    fig.update_yaxes(title_text="Price ($US)", row=1, col=1)
+    fig.update_yaxes(title_text="Revenue ($US Millions)", row=2, col=1)
+    fig.update_layout(showlegend=False,
+    height=900,
+    title=stock,
+    xaxis_rangeslider_visible=True)
+    fig.show()
+#%% Use yfinance to Extract Stock Data
+tesla =yf.Ticker("TSLA")
+tesla_data=tesla.history(period="max")
+tesla_data.reset_index(inplace=True)
+tesla_data.head(5)
+# Use Webscraping to Extract Tesla Revenue Data
+url="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/revenue.htm"
+html_data=requests.get(url).text
+soup = BeautifulSoup(html_data, 'html5lib')
+
+tesla_revenue = pd.DataFrame(columns=["Date","Revenue"])
+for row in soup.find_all("tbody")[1].find_all("tr"):
+    col = row.find_all("td")
+    Date = col[0].text
+    Revenue = col[1].text#ADD_CODE
+    tesla_revenue =tesla_revenue.append({"Date":Date, "Revenue":Revenue}, ignore_index=True)
+    
+tesla_revenue["Revenue"] = tesla_revenue['Revenue'].str.replace(',|\$',"")    
+tesla_revenue.dropna(inplace=True)
+tesla_revenue = tesla_revenue[tesla_revenue['Revenue'] != ""]
+tesla_revenue.tail(5)
+#%%
+make_graph(tesla_data,tesla_revenue,"Tesla")
